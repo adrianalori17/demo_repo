24f979b7b5103b1ffcaf114e3563daf2f9fbb956
adrianalori17 <adriana.lori@cancer.org>
2026-02-20 22:04:09 -0500

    Update README

---
+++ # BEGIN: Sample R Script.r
@@ -0,0 +1,13 @@
+# BEGIN: Sample R Script
+# This script calculates the sum of two numbers
+
+# Define two numbers
+num1 <- 5
+num2 <- 10
+
+# Calculate the sum
+sum <- num1 + num2
+
+# Print the result
+print(paste("The sum of", num1, "and", num2, "is:", sum))
+# END: Sample R Script
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # demo_repo
 Test demo
+Change This 
+Create a small script using copilot
